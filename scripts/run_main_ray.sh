947a9e5db9f3e141b532682c276a36f92daf8d1a
simomoxy <133643714+simomoxy@users.noreply.github.com>
2024-09-23 09:01:12 +0200

    Update run_main_ray.sh

--- scripts/run_main_ray.sh
+++ scripts/run_main_ray.sh
@@ -5,9 +5,9 @@ DSR=0.0
 DATA_SEED=0
 MODEL='cnn'
 SOTA_CONFIG=False
-CONFIG_PATH="/Users/simonjaxy/Documents/vub/WP1/limited_data/ray/cnn/top1/single/ASHA/False/0.0/0/top1.pkl"
+CONFIG_PATH="./ray/cnn/top1/single/ASHA/False/0.0/0/top1.pkl"
 EPOCHS=1
 
-DATA_SET='/Users/simonjaxy/Documents/vub/WP1/ssm_ecg/data/ptb_xl_fs100'
+DATA_SET='./data/ptb_xl_fs100'
 
 python ./code/train_ecg_model.py  --dataset="${DATA_SET}" --epochs=${EPOCHS} --data_seed=${DATA_SEED} --config_path="${CONFIG_PATH}" --id=${ID} --device=${DEVICE} --accelerator="${ACCELERATOR}" --down_sample_rate=${DSR} --logdir ./logs/"${MODEL}" --model "${MODEL}"
