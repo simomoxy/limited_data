da714253fc84d7029a9cc8a4b42ba0c7ec7dd5dc
simomoxy <133643714+simomoxy@users.noreply.github.com>
2024-09-23 09:01:40 +0200

    Update run_ray.sh

--- scripts/run_ray.sh
+++ scripts/run_ray.sh
@@ -13,6 +13,6 @@ STUDY_NAME="ray_${MODEL}_${OBJECTIVE}_${OPTIMIZATION}_${DSR}_${SOTA_CONFIG}_${DA
 NUM_STUDY_SAMPLES=1
 EPOCHS=1
 
-DATA_SET='/Users/simonjaxy/Documents/vub/WP1/ssm_ecg/data/ptb_xl_fs100'
+DATA_SET='./data/ptb_xl_fs100'
 
 python ./code/tune_ray.py --dataset="${DATA_SET}" --num_study_samples "${NUM_STUDY_SAMPLES}" --data_seed=${DATA_SEED} --study_name="${STUDY_NAME}" --objectives="${OBJECTIVE}" --optimization="${OPTIMIZATION}" --num_worker=${NUM_WORKER} --gpus_per_worker=${GPUS_PER_WORKER} --cpus_per_worker=${CPUS_PER_WORKER} --epochs "${EPOCHS}" --device ${DEVICE} --accelerator="${ACCELERATOR}" --down_sample_rate=${DSR} --study_path ./ray/"${MODEL}" --logdir ./logs/ray/"${MODEL}"  --model "${MODEL}"
