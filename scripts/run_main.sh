63a021c21ea82ff26045bd97f338e41579d67338
simomoxy <133643714+simomoxy@users.noreply.github.com>
2024-09-23 09:00:26 +0200

    Update run_main.sh
    changed data path

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -7,6 +7,6 @@ CONFIG_PATH=""
 ACCELERATOR='cpu'
 EPOCHS=1
 
-DATA_SET='/Users/simonjaxy/Documents/vub/WP1/ssm_ecg/data/ptb_xl_fs100'
+DATA_SET='./data/ptb_xl_fs100'
 
 python ./code/train_ecg_model.py  --dataset="${DATA_SET}" --epochs=${EPOCHS} --data_seed=${DATA_SEED} --config_path="${CONFIG_PATH}" --id=${ID} --accelerator="${ACCELERATOR}" --device=${DEVICE} --down_sample_rate=${DSR} --logdir ./logs/"${MODEL}" --model "${MODEL}"
